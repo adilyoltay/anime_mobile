a41b2a1e9d2250423505e56f58f5576f93bfef22
Adil Yoltay <adil@anime-mobile.dev>
2025-10-04 13:13:05 +0300

    Fallback to legacy one-based binding indices

--- converter/src/universal_builder.cpp
+++ converter/src/universal_builder.cpp
@@ -1939,14 +1939,18 @@ CoreDocument build_from_universal_json(const nlohmann::json& data, PropertyTypeM
                     uint16_t inputIndex = ctxInfo.originalPath[1];
                     CoreObject* inputCore = nullptr;
 
-                    if (inputIndex < smBinding.inputList.size()) {
-                        inputCore = smBinding.inputList[inputIndex];
+                    if (inputIndex == 0) {
+                        if (!smBinding.inputList.empty()) {
+                            inputCore = smBinding.inputList[0];
+                        }
                     }
-
-                    if (inputCore == nullptr && inputIndex > 0) {
-                        size_t zeroIndex = static_cast<size_t>(inputIndex - 1);
-                        if (zeroIndex < smBinding.inputList.size()) {
-                            inputCore = smBinding.inputList[zeroIndex];
+                    else {
+                        size_t legacyIndex = static_cast<size_t>(inputIndex - 1);
+                        if (legacyIndex < smBinding.inputList.size()) {
+                            inputCore = smBinding.inputList[legacyIndex];
+                        }
+                        else if (inputIndex < smBinding.inputList.size()) {
+                            inputCore = smBinding.inputList[inputIndex];
                         }
                     }
 
